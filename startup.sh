266a52f4c9411bf9920b566617a1a14c2157d1df
Joel Jothiprakasam <hijoelj@gmail.com>
2022-12-20 16:08:19 -0800

    Save and kill old bot process when starting

--- startup.sh
+++ startup.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
-nohup python3 graderoomba.py &
+ID_FILE=.id
+ID=""
+if [ -f "$ID_FILE" ]
+then
+    ID=$(<.id)
+fi
+
+if [ -z "$ID" ]
+then
+    echo "No stored Graderoomba process ID"
+else
+    echo "Killing existing Graderoomba process $ID"
+    kill -9 $ID
+fi
+
+nohup python3 graderoomba.py > .out 2> .err < /dev/null &
+echo "$!" > .id
+echo "Started Graderoomba process with ID $!"

3c19661722dd0d3cdfc8f36a5384510b7e3fafe3
Bryce Hackel <brycemhackel@gmail.com>
2023-01-08 15:07:38 -0800

    Add support for getting beta role(s)

--- graderoomba.py
+++ graderoomba.py
@@ -14,9 +14,8 @@ dotenv.load_dotenv()
 GRADEROOM_KEY = os.environ.get("GRADEROOM_KEY")
 DISCORD_KEY = os.environ.get("DISCORD_KEY")
 MAIN_GUILD_ID = os.environ.get("MAIN_GUILD_ID")
-IS_BETA = (os.environ.get("IS_BETA").lower() == "true")
 
-# Set Intents, do we need message content for commands?
+# Set Intents
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -29,9 +28,10 @@ tree = app_commands.CommandTree(client)
 VERIFY_DESC = "Enter your Graderoom username to verify yourself"
 ROLES_DESC = "After verification, run this command to get roles"
 guild = discord.Object(id=MAIN_GUILD_ID)
-beta_str = 'beta.' if IS_BETA else ''
-API_CONNECT_URL = f'https://{beta_str}graderoom.me/api/internal/discord/connect'
-API_USER_INFO_URL = f'https://{beta_str}graderoom.me/api/internal/discord/user-info'
+API_CONNECT_URL = f'https://graderoom.me/api/internal/discord/connect'
+API_USER_INFO_URL = f'https://graderoom.me/api/internal/discord/user-info'
+BETA_API_CONNECT_URL = f'https://beta.graderoom.me/api/internal/discord/connect'
+BETA_API_USER_INFO_URL = f'https://beta.graderoom.me/api/internal/discord/user-info'
 HEADERS = {'x-internal-api-key': GRADEROOM_KEY}
 ERROR_CODES = {
     1: "Invalid Discord ID",
@@ -40,8 +40,8 @@ ERROR_CODES = {
     4: "The specified Graderoom account already has a linked Discord account.",
     5: "You must connect your Discord account before you can get roles."
 }
-SUCCESS_MSG = "Your pairing code is **{}**. Type it into the notification on your Graderoom account. Your code expires" \
-              " in 2 minutes."
+SUCCESS_MSG = "Your pairing code is **{}**. Type it into the notification on your Graderoom account. Your code " \
+              "expires in 2 minutes. "
 TIMESTAMP_STR = '%a %H:%M:%S'
 USER_ROLE_ID = 898395943030906912
 SCHOOL_ROLE_IDS = {
@@ -49,43 +49,61 @@ SCHOOL_ROLE_IDS = {
     'basis': 898398946118996098,
     'ndsj': 1038570324775686178
 }
+BETA_TESTER_ROLE_ID = 897624644306239488
 
 
 # Verify command to start pairing between Discord ID and Graderoom account
 @tree.command(name="verify", description=VERIFY_DESC, guild=guild)
-async def verify_command(interaction, graderoom_username: str) -> None:
+async def verify_command(interaction, graderoom_username: str, beta: bool = False) -> None:
     discord_id = interaction.user.id
+    # Log the request
+    site_type = "beta" if beta else "stable"
     timestamp = time.strftime(TIMESTAMP_STR)
-    print(f"[{timestamp}] {discord_id} requested verification for {graderoom_username}")
+    print(f"[{timestamp}] {discord_id} requested {site_type} verification for {graderoom_username}")
     # Set up and call API
+    url = BETA_API_CONNECT_URL if beta else API_CONNECT_URL
     body = {'username': graderoom_username, 'discordID': discord_id}
-    resp = requests.post(API_CONNECT_URL, headers=HEADERS, json=body)
+    resp = requests.post(url, headers=HEADERS, json=body)
     json_resp = resp.json()
 
-    # Reply with response error or the verification code
+    # Reply with response error
     if not resp.ok:
         message = ERROR_CODES[json_resp['errorCode']]
 
         timestamp = time.strftime(TIMESTAMP_STR)
         print(f"[{timestamp}] {discord_id} received error: {message}")
         await interaction.response.send_message(message, ephemeral=True)
-    else:
-        verification_code = json_resp['verificationCode']
-        timestamp = time.strftime(TIMESTAMP_STR)
-        print(f"[{timestamp}] {discord_id} received pairing code {verification_code}")
-        await interaction.response.send_message(SUCCESS_MSG.format(verification_code), ephemeral=True)
+        return
+
+    # Reply with the 2-digit verification code
+    verification_code = json_resp['verificationCode']
+    timestamp = time.strftime(TIMESTAMP_STR)
+    print(f"[{timestamp}] {discord_id} received pairing code {verification_code}")
+    await interaction.response.send_message(SUCCESS_MSG.format(verification_code), ephemeral=True)
 
 
 # Roles command to give roles to the Discord user
 @tree.command(name="roles", description=ROLES_DESC, guild=guild)
-async def roles_command(interaction) -> None:
+async def roles_command(interaction, beta: bool = False) -> None:
     discord_id = interaction.user.id
+    # Log the request
+    site_type = "beta" if beta else "stable"
     timestamp = time.strftime(TIMESTAMP_STR)
-    print(f"[{timestamp}] {discord_id} requested roles")
+    print(f"[{timestamp}] {discord_id} requested {site_type} roles")
     # Set up and call API
+    url = BETA_API_USER_INFO_URL if beta else API_USER_INFO_URL
     body = {'discordID': discord_id}
-    resp = requests.get(API_USER_INFO_URL, headers=HEADERS, json=body)
+    resp = requests.get(url, headers=HEADERS, json=body)
     json_resp = resp.json()
+
+    # Errors, mainly if user has not linked yet
+    if not resp.ok:
+        message = ERROR_CODES[json_resp['errorCode']]
+
+        timestamp = time.strftime(TIMESTAMP_STR)
+        print(f"[{timestamp}] {discord_id} received error on {site_type}: {message}")
+        await interaction.response.send_message(message, ephemeral=True)
+        return
 
     roles_to_add = []
     # Add role for school
@@ -95,6 +113,10 @@ async def roles_command(interaction) -> None:
     # Add User role
     role = interaction.guild.get_role(USER_ROLE_ID)
     roles_to_add.append(role)
+    # Add role for beta
+    if beta:
+        role = interaction.guild.get_role(BETA_TESTER_ROLE_ID)
+        roles_to_add.append(role)
     # todo add roles for donations
 
     # Give roles and respond to message
@@ -102,7 +124,7 @@ async def roles_command(interaction) -> None:
         await interaction.user.add_roles(role)
     response = "Roles given: " + ", ".join([x.name for x in roles_to_add])
     timestamp = time.strftime(TIMESTAMP_STR)
-    print(f"[{timestamp}] {discord_id} received roles: {response}")
+    print(f"[{timestamp}] {discord_id} received roles, {response}")
     await interaction.response.send_message(response, ephemeral=True)
 
 
